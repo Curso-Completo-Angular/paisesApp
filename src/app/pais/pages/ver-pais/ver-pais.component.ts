1c7023cd26c3a2a82ceb75ef3b5596163016895f
wnicolas <nicolas.buitrago2@gmail.com>
2022-07-10 14:07:45 -0500

    switchMap RxJs

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.paisService.getPaisPorCodigo(id)))
+      .subscribe((resp) => {
+        console.log(resp);
+      });
+    // this.activateRoute.params.subscribe(({ id }) => {
+    //   this.paisService.getPaisPorCodigo(id).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
   }
-
 }
