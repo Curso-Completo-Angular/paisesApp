9c310d3cdbeabf6a6a79a1cbb1857121c358047e
wnicolas <nicolas.buitrago2gmail.com>
2022-07-18 13:51:36 -0500

    Paises temriando

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,37 +1,52 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/pais.interface';
-import { PaisService } from '../../services/pais.service';
+import { Component } from '@angular/core'
+import { Country } from '../../interfaces/pais.interface'
+import { PaisService } from '../../services/pais.service'
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   constructor(private paisService: PaisService) {}
 
-  termino: string = '';
-  hayError = false;
-  paises: Country[] = [];
+  termino: string = ''
+  hayError = false
+  paises: Country[] = []
+  paisesSugeridos: Country[] = []
 
   buscar(termino: string) {
-    this.hayError = false;
-    this.termino = termino;
-    console.log(this.termino);
+    this.hayError = false
+    this.termino = termino
+    console.log(this.termino)
     this.paisService.buscarPais(this.termino).subscribe(
       (paises) => {
-        console.log(paises);
-        this.paises = paises;
+        console.log(paises)
+        this.paises = paises
       },
       (err) => {
-        this.hayError = true;
-        this.paises = [];
-        console.log(err);
-        console.info(err);
-      }
-    );
+        this.hayError = true
+        this.paises = []
+        console.log(err)
+        console.info(err)
+      },
+    )
   }
   sugerencias(termino: string) {
-    this.hayError = false;
+    this.hayError = false
+    this.paisService.buscarPais(termino).subscribe(
+      (res) => {
+        this.paisesSugeridos = res
+      },
+      (err) => {
+        this.paisesSugeridos = []
+      },
+    )
   }
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { PaisService } from '../../services/pais.service'
+import { Country } from '../../interfaces/pais.interface'
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorRegionComponent implements OnInit {
+  regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  regionActiva: string = ''
+  paises: Country[] = []
+  constructor(private paisService: PaisService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  activarRegion(region: string) {
+    this.regionActiva = region
+    this.paisService.getPaisesPorRegion(region).subscribe((response) => {
+      this.paises = response
+      console.log(this.paises)
+    })
   }
-
 }
